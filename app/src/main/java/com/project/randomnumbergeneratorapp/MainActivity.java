32e0681c17e25a54dc5d2b11391c99af7690fb8b
Randika Ranatunga <75309593+RDR1997@users.noreply.github.com>
2023-03-07 16:52:20 +0530

    finished

--- app/src/main/java/com/project/randomnumbergeneratorapp/MainActivity.java
+++ app/src/main/java/com/project/randomnumbergeneratorapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.project.randomnumbergeneratorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.generate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editTextRange);
+                TextView textView = (TextView) findViewById(R.id.displayNumber);
+
+                int range = Integer.parseInt(editText.getText().toString());
+                Random ran = new Random();
+                int rand_no = ran.nextInt(range-0) + 0;
+
+                textView.setText(rand_no + " ");
+            }
+        });
     }
 }
